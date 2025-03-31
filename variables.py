3860555855ab7f347c8ca735741edbd22ce9016f
Muzammil <mformuzzi@gmail.com>
2025-03-31 11:57:59 +0500

    Initial Commit

---
+++ variables.py
@@ -0,0 +1,53 @@
+# Introduction
+
+# Python is a high level programming language which is developed by Guido Van Rossum in 1991 . Python is
+# used in web-development , app-development , data-science , AI , ML etc . It is a esay language which
+# can be learn by Anyone . Working with python is  like working in English . Due to this feature . 
+# Beginners love this languaga
+
+
+# Variable :
+    # A variable is a container to store some values or datatypes . Perhaps there is some rules for 
+    # creating variables . These are 
+
+# 1 . Variables can not start with numbers
+# 2 . A variable name can contain alphanumerice number (A-Z , 0-9)
+# 3 . Variable name are case sensitive means "avg" and "Avg" are separate Variables 
+
+# Note :
+    # In the world of Programming and Development . We always gives those name to varaibles which can 
+# make sense or align with the work we done . so tht it help others to understand what we are doing
+# in it .   
+
+# value = 1
+# print(value)
+
+# Comments :
+    # Suppose we want to document our code . but when we write document and run it with code . It throw 
+    # error here comments come . We use it to document our code without getting Error . There are 2
+    # types of comment
+
+# 1 . Single Line Comment (#)
+
+# This is comment 
+# a = 10  
+
+# 2 . Multiline Comment (''')
+
+'''
+This is
+Multi line
+comment
+'''
+
+
+
+
+
+
+
+
+
+
+
+
