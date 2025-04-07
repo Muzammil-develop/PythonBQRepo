1adeba50fd41d74ac6ec3ffdd0a0dc3656de0e7e
Muzammil <mformuzzi@gmail.com>
2025-04-07 08:05:57 +0500

    Numbers started

---
+++ python_numbers.py
@@ -0,0 +1,61 @@
+# Definition:
+    # There are 3 different types of numeric types in python
+    # 1 . Integer
+    # 2 . Float
+    # 3 . complex
+
+    # Variables of numeric type is created we we assign values to its , like this
+    # a = 10
+
+    # To verify the type of object we use type function
+
+# 1 . Integer :
+    # Interger is a whole number of positive or negative values without having decimal and can be 
+    # of unlimited length
+
+# x = 1
+# y = 52489674751
+# z = -250
+
+
+# print(x)
+# print(y)
+# print(z)
+
+# 2 . Float :
+    # Float is a number containing positive and negative values with 1 or more decimal values
+
+# x = 2.5
+# y = -35.95
+# z = 2.2
+
+# print(x)
+# print(y)
+# print(z)
+
+    # Float can also be  scientific number with an 'e' to indicate the power of 10
+
+# x = 35e3
+# y = 12E4
+# z = -87.7e100
+
+# print(x)
+# print(y)
+# print(z)
+
+# 3 . Complex :
+    # Complex numbers are written with a 'j' as imaginary part
+
+# x = 3 + 5j
+# y = 8j
+# z = -65j
+
+# print(x)
+# print(y)
+# print(z)
+
+ 
+
+
+
+
